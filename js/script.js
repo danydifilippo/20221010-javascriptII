1a05163147f96478fbe863093fa51a9a8f610595
danydifilippo <danydifilippo@gmail.com>
2022-10-10 17:08:51 +0200

    finally

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-// function calcola(a) {
-//     let dataTime = new Date();
-//     let data = dataTime.getFullYear();
-//     let mese = dataTime.getMonth();
-//     a = new Date(a)
-//     let yearBirth=a.getFullYear()
-//     let monthBirth=a.getMonth()
-//     let year_ = Number(data - yearBirth);
-//     let age;
-//     if(mese<=monthBirth){
-//         age = year_
-//     } else {
-//         age = Number (year_-=1)
-//     }
-//     return age
-// }
+let dataTime = new Date();
+let data = dataTime.getFullYear();
+let mese = dataTime.getMonth();
+
+function calcola(a) {
+    a = new Date(a)
+    let yearBirth=a.getFullYear()
+    let monthBirth=a.getMonth()
+    let year_ = Number(data - yearBirth);
+    let age;
+    if(mese<=monthBirth){
+        age = year_
+    } else {
+        age = Number (year_-=1)
+    }
+    return age
+}
 
 class Persona {
     constructor (nome, cognome, dataNascita) {
     this.nome=nome;
     this.cognome=cognome;
     this.dataNascita=dataNascita;
-    // this.eta= Number(calcola(dataNascita));
+    this.eta= Number(calcola(dataNascita));
 }
 }
 
@@ -33,7 +34,7 @@ function inserisci(a,b,c){
     let iscritto=new Persona(a,b,c);
     let iscrLista=document.querySelector('tbody');
     let row=document.createElement('tr');
-    row.innerHTML=`<td>${iscritto.nome}</td><td>${iscritto.cognome}</td><td>${iscritto.dataNascita}</td>`
+    row.innerHTML=`<td>${iscritto.nome}</td><td>${iscritto.cognome}</td><td>${iscritto.dataNascita}</td><td>${iscritto.eta}</td>`
     iscrLista.appendChild(row);
 }
 
